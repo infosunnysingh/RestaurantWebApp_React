7365bd90384a1a1dcecd81563a424804b44c630b
Sunny Singh <sunny.singh@calsoftinc.com>
2023-02-13 22:19:37 +0530

    Modifications

---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,86 @@
+const Menu = [
+  {
+    id: 1,
+    image: "images/maggi.jpg",
+    name: "maggi",
+    category: "breakfast",
+    price: "12₹",
+    description:
+      "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+
+  {
+    id: 2,
+    image: "images/allupakoida.jpg",
+    name: "allu pakoida",
+    category: "evening",
+    price: "20₹",
+    description:
+      "Aloo pakora are soft melt-in-the-mouth fried potato fritters coated in a delicious, golden, crispy batter. It is also known as potato bajji and has always been one of my favorite tea-time snacks.",
+  },
+  {
+    id: 3,
+    image: "images/corn.jpg",
+    name: "corn",
+    category: "breakfast",
+    price: "10₹",
+    description:
+      "Corn is a tall annual cereal grass (Zea mays) that is widely grown for its large elongated ears of starchy seeds.",
+  },
+  {
+    id: 4,
+    image: "../images/chola.jpg",
+    name: "chola",
+    category: "lunch",
+    price: "50₹",
+    description:
+      "The Chola dynasty was a Tamil thalassocratic empire of southern India and one of the longest-ruling dynasties in the history of the world.",
+  },
+  {
+    id: 5,
+    image: "../images/pizza.jpg",
+    name: "pizza",
+    category: "evening",
+    price: "80₹",
+    description:
+      "Pizza is full of umami, which means “delicious” in Japanese. Just like the other types of taste, which are sweet, sour, salty, and bitter, umami is a rich and savory flavor that your taste buds taste and recognize.",
+  },
+  {
+    id: 6,
+    image: "../images/nonvegthali.jpg",
+    name: "Non-Veg Thali",
+    category: "dinner",
+    price: "180₹",
+    description:
+      "Traditional meal served with chicken tikka masala, lamb curry, tandoori chicken, samosa, rice, plain naan and rice pudding.",
+  },
+  {
+    id: 7,
+    image: "../images/sweet.jpg",
+    name: "Sweets",
+    category: "dinner",
+    price: "60₹",
+    description:
+      "Sweets like candy, soft drinks, syrups, jams, and jellies are made mostly of sugars. Most of these foods do not contain many vitamins or minerals. So they are called ",
+  },
+  {
+    id: 8,
+    image: "../images/rajmarice.jpg",
+    name: "Rajma Rice",
+    category: "lunch",
+    price: "60₹",
+    description:
+      "Rajma chawal is a rice dish where cooked rice is mixed with rajma masala or kidney beans masala. A comfort food that comes together in a jiffy.",
+  },
+  {
+    id: 9,
+    image: "../images/samosa.jpg",
+    name: "samaso",
+    category: "evening",
+    price: "10₹",
+    description:
+      "A samosa is a fried South Asian pastry with a savoury filling, including ingredients such as spiced potatoes, onions, and peas. It may take different forms, including triangular, cone, or half-moon shapes, depending on the region.",
+  },
+];
+
+export default Menu;
